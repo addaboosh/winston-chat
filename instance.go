9bff7675ee940b8b2404c7e7ca9bbf8db9d38e45
addaboosh <127968482+addaboosh@users.noreply.github.com>
2024-07-20 21:42:15 +0100

    added three methods and err handling to instance.go

--- instance.go
+++ instance.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"log"
 
 	"github.com/google/uuid"
@@ -15,10 +17,30 @@ func NewInstance(l *log.Logger) *Instance {
 	return &Instance{l, map[uuid.UUID]Worker{}}
 }
 
-func (i *Instance) AddWorker(w *Worker) {
-	i.workers[w.id] = *w
+func (i *Instance) GetWorker(id uuid.UUID) (*Worker, error) {
+
+	w, ok := i.workers[id]
+	if ok {
+		return &w, nil
+	}
+	return nil, errors.New(fmt.Sprintf("Could not find Worker id %v", id))
 }
-func (i *Instance) RemoveWorker(id uuid.UUID){
+
+func (i *Instance) CreateWorker() (id uuid.UUID, err error) {
+	w, err := NewWorker()
+	if err != nil {
+		return uuid.UUID{}, err
+	}
+	i.workers[w.id] = *w
+	return w.id, nil
+}
+
+func (i *Instance) RemoveWorker(id uuid.UUID) error {
 	// ToDo - Need to check if any active connections before deleting
 	delete(i.workers, id)
+	_, ok := i.workers[id]
+	if ok { 
+		return nil
+	}
+	return errors.New(fmt.Sprintf("Worker id: %v not removed", id))
 }
--- main.go
+++ main.go
@@ -1,39 +1,10 @@
 package main
 
-import (
-	"github.com/addaboosh/winston-chat/handlers"
-	"github.com/google/uuid"
-	"github.com/gorilla/websocket"
-	"log"
-	"net/http"
-)
+import "fmt"
 
 func main() {
-	mux := http.NewServeMux()
-
-	mux.Handle("/", handlers.NewHello())
-
-	http.ListenAndServe(":8123", mux)
 }
 
-type Instance struct {
-	l *log.Logger
-	w map[uuid.UUID]Worker
-}
 
-type Worker struct {
-	id       uuid.UUID
-	name     string
-	conns    map[uuid.UUID]*websocket.Conn
-	channels []string
-	groups   Group
-}
 
-func NewWorker() *Worker {
-	return {
-		id uuid.UUID. 
 
-	}
-}
-
-type Group map[string][]uuid.UUID
